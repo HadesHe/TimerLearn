c385a8518e4663c3d798feab56aaf2cdbcf1f8ac
hezhanghe <hezhanghe@hzjytech.com>
2018-01-02 19:44:06 +0800

    start apply

--- kotlintips/src/test/java/com/example/kotlintips/StringTip/StringTips.kt
+++ kotlintips/src/test/java/com/example/kotlintips/StringTip/StringTips.kt
@@ -14,7 +14,7 @@ class StringTipsTest {
                     line2
                     line3
                     """
-        val js="""
+        val js = """
             function myFunction()
             {
                 document.getElementById("demo")
@@ -27,39 +27,68 @@ class StringTipsTest {
     }
 
     @Test
-    fun testString2(){
-        val strings= arrayListOf<String>("abc","def","ghi")
+    fun testString2() {
+        val strings = arrayListOf<String>("abc", "def", "ghi")
         println("First content is $strings")
         println("First content is ${strings[0]}")
-        println("First content is ${if (strings.size>0) strings[0] else "null"}")
+        println("First content is ${if (strings.size > 0) strings[0] else "null"}")
 
     }
 
     @Test
-    fun showDoll(){
+    fun showDoll() {
         println("First content is \$strings")
         println("First content is ${'$'}strings")
     }
 
-    fun String.lastChar(): Char=this.get(this.length-1)
-
+    fun String.lastChar(): Char = this.get(this.length - 1)
 
 
     @Test
-    fun testFunExtension(){
-//        val str="test function fun"
+    fun testFunExtension() {
+        //        val str="test function fun"
 //        println(str.lastChar())
-        fun testIf()= if(false) 1 else null
+        fun testIf() = if (false) 1 else null
         println(testIf())
     }
 
+
+    val s = with(StringBuilder()) {
+        append("TAG")
+        for (i in 'A'..'Z') {
+            append(i)
+        }
+        this
+    }
+
+
+    data class User(var name: String, var password: String)
+
     @Test
     fun testIfFunction() {
-        if(true){
-            println("1")
-        }else{
-            println("2")
+        var user = User("111", "2222")
+        println(user)
+
+        with(user) {
+            name = "2222"
+            password = "3333"
+
         }
-        println(3)
+        println(user)
+
+        with(user,{
+            name = "333"
+            password = "4444"
+
+        })
+
+        println(user)
+
+
     }
+
+//    public inline fun <T>  T.apply(block: T.() → Unit):T
+//   public inline fun <T, R> with(receiver: T) {block: T.() → R}: R
+//   public inline fun <T, R> with(receiver: T, block: T.() → R): R
+
 }
