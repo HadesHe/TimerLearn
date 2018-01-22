c6e4864616201983cdaa568a4bff6ebe5bd90c56
zhanghehe <hezhanghe@126.com>
2018-01-22 23:01:46 +0800

    update room version

--- app/src/main/java/com/hzjytech/hades/timerlearn/data/source/local/ToDoDataBase.kt
+++ app/src/main/java/com/hzjytech/hades/timerlearn/data/source/local/ToDoDataBase.kt
@@ -1,12 +1,13 @@
 package com.hzjytech.hades.timerlearn.data.source.local
 
 import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
 import com.hzjytech.hades.timerlearn.data.Task
 
 /**
  * Created by zhanghehe on 2018/1/21.
  */
 @Database(entities = arrayOf(Task::class),version = 1)
-abstract class ToDoDataBase : RoomDatabase() {
+abstract class ToDoDataBase: RoomDatabase(){
 
 }
